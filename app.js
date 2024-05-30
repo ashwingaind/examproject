a70c70f3a0625984a37bc78a271c7a9c5658d32a
ashwingaind <eaa23asga@students.eaaa.dk>
2024-05-31 01:10:54 +0200

    New Code

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const projects = await getProjects(); // Corrected the function name
+  console.log(projects);
+  // displayProjects(projects);
+  displayProjectsGrid(projects);
+}
+
+async function getProjects() {
+  const response = await fetch("https://wordpressexam.poolofdesign.dk/wp-json/wp/v2/projects?acf_format=standard");
+  const data = await response.json();
+  return data;
+}
+
+
+function displayProjectsGrid(projects) {
+  const projectsGrid = document.querySelector("#project-grid");
+  for (const project of projects) { // Corrected the parameter name in the for...of loop
+    projectsGrid.insertAdjacentHTML(
+      "beforeend",
+      `    
+    <article class="grid-item">
+      <img src="${project.acf.image}" alt="${project.title.rendered}"></img>
+      <h2>${project.title.rendered}</h2>
+      <p>${project.acf.description}</P>
+    </article>
+    `
+    );
+  }
 }
